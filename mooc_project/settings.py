f9e1b4f522dd433c615202f65361fef42878e819
Richard Rozario A <richardrozario2000@gmail.com>
2023-02-23 03:17:56 +0530

    changes for deployment

--- mooc_project/settings.py
+++ mooc_project/settings.py
@@ -95,6 +95,21 @@ TEMPLATES = [
     },
 ]
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'INFO',
+    },
+}
+
+
 WSGI_APPLICATION = 'mooc_project.wsgi.application'
 
 # Database
